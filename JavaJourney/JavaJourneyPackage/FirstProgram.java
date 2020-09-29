a69199a564677f3b46e834f51823c08c59bb7f00
mohkhan9 <mohkhan9@host.docker.internal>
2020-09-28 21:17:28 -0400

    changed the message-9:16 PM.

--- JavaJourney/JavaJourneyPackage/FirstProgram.java
+++ JavaJourney/JavaJourneyPackage/FirstProgram.java
@@ -4,7 +4,7 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is a test");
+		System.out.println("this is a Farooq Khan");
 	}
 
 }
